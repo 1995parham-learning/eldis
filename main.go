1e91a68873e62510e814e36c8086dc7c205b7b3f
Parham Alvani <parham.alvani@gmail.com>
2023-03-22 22:42:04 +0330

    feat: update main with new command module

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/1995parham-learning/eldis/internal/command"
 	"github.com/1995parham-learning/eldis/internal/redis"
@@ -17,33 +15,21 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		c, err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		c = strings.TrimSuffix(c, "\n")
+		cmd, err := command.Parse(input)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		commands := strings.Split(c, " ")
-
-		if commands[0] == command.SET {
-			if err := redis.SetValidation(commands); err != nil {
-				fmt.Println(err.Error())
-
-				continue
-			}
-
-			r.Set(commands[1], commands[2])
-		} else if commands[0] == command.GET {
-			if err := redis.GetValidation(commands); err != nil {
-				fmt.Println(err.Error())
-
-				continue
-			}
-
-			fmt.Println(r.Get(commands[1]))
-		} else {
-			fmt.Println("Just SET and GET commands are valid")
+		switch t := cmd.(type) {
+		case command.Get:
+			r.Get(t.Key)
+		case command.Set:
+			r.Set(t.Key, t.Value)
 		}
 	}
 }
